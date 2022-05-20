e752746fa80ab987f5ce6e1697b3567e4f7845d1
Alexis Toledo <alexis.toledo@live.com>
2022-05-20 13:05:20 +0200

    adds comments

--- src/lib/components/SubmitButton.jsx
+++ src/lib/components/SubmitButton.jsx
@@ -3,9 +3,17 @@ import { ReactComponent as CheckIcon } from "./check.svg";
 import "./SubmitButton.sass";
 
 const SubmitButton = ({ type, size, disabled, onButtonClick }) => {
+    /**
+     * State
+     *  buttonSize @type {String} - Default value: "medium"
+     *  variant @type {String} - Default value: "default"
+     *  */
     const [variant, setVariant] = useState("default");
     const [buttonSize, setButtonSize] = useState("medium");
 
+    /**
+     *  Sets the value of the variant on the state
+     */
     const getVariant = () => {
         switch (type) {
             case "default":
@@ -19,6 +27,9 @@ const SubmitButton = ({ type, size, disabled, onButtonClick }) => {
         }
     };
 
+    /**
+     *  Sets the value of the size on the state
+     */
     const getButtonSize = () => {
         switch (size) {
             case "small":
@@ -32,6 +43,9 @@ const SubmitButton = ({ type, size, disabled, onButtonClick }) => {
         }
     };
 
+    /**
+     *  Sets the value of the variant on the state
+     */
     const triggerButton = (e) => {
         if (e.keyCode === 83 && !disabled) {
             onButtonClick();
